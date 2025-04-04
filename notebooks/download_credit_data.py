e6c36a1dc1583a8dbe5c33f0f60ea1e4d6c88308
Harsh50116 <imharsh5555@gmail.com>
2025-04-04 12:26:38 -0400

    feat: Project setup and data cleaning

--- notebooks/download_credit_data.py
+++ notebooks/download_credit_data.py
@@ -1,15 +1,16 @@
-from datasets import load_dataset
+#%%
 import pandas as pd
 
-# Load the dataset
-dataset = load_dataset("scikit-learn/credit-card-clients", split='train')
-
-# Convert to a pandas DataFrame
-df = dataset.to_pandas()
+df = pd.read_csv("credit_card_clients_full.csv")
 
 # Display the number of rows and the first few entries
 print(f"Total rows: {len(df)}")
 print(df.head())
 
-# Save the DataFrame to a CSV file
-df.to_csv("../data/credit_card_clients_full.csv")
+# %%
+
+# df.info()
+print(df.isnull().sum())
+# %%
+
+
